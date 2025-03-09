da486d7e0a851cce8199d6169b91d29e094c80ca
Raghav Nayak <ragsnayak@gmail.com>
2025-03-09 22:40:15 +0530

    worker pools examples

---
+++ src/40_worker_pools/workerPools.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs { // workers receive work on jobs channel
+		fmt.Println("worker", id, "started job", j)
+		time.Sleep(time.Second) // if we don't have this, all the jobs are handled by a single worker
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2 // send the results to the results channel
+	}
+}
+
+func main() {
+	const numsJobs = 5
+	jobs := make(chan int, numsJobs)
+	results := make(chan int, numsJobs)
+
+	// to run concurrent workers; i.e. 3 workers
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// sending 5 jobs
+	for j := 1; j <= numsJobs; j++ {
+		jobs <- j
+	}
+	close(jobs) // clone the channel to indicate the work we have
+
+	// this is important to collect all the results of the work to ensure that the worker goroutines have finished
+	// alternative to this is to use WaitGroup
+	for a := 1; a <= numsJobs; a++ {
+		<-results
+	}
+}
+
+// output:
+// note: even though we have 5 jobs, the concurrent workers have finished the jobs in 2s
+// worker 3 started job 1
+// worker 1 started job 2
+// worker 2 started job 3
+// worker 3 finished job 1
+// worker 3 started job 4
+// worker 1 finished job 2
+// worker 1 started job 5
+// worker 2 finished job 3
+// worker 3 finished job 4
+// worker 1 finished job 5
